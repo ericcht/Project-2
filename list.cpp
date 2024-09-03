d3e727090912165bbf12e2b722f93ffc8c14b436
Eric Jeliaz Chtilianov <echtilia@hydra10.eecs.utk.edu>
2024-09-03 11:49:25 -0400

    All files needed

---
+++ list.cpp
@@ -0,0 +1,27 @@
+#include "volsort.h"
+
+/*struct List {
+    Node       *head;
+    size_t      size;
+
+    List(); 					// define in list.cpp
+    ~List();					// define in list.cpp
+
+  //    void push_front(const std::string &s);	//define below
+};*/
+
+List::List()
+{
+    head = NULL;
+    size = 0;
+}
+
+List::~List()
+{
+    Node *current = head;
+    while (current != NULL){
+        Node *next = current->next;
+        delete current;
+        current = next;
+    }
+}
