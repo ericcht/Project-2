30344e71ba1fd87e81a828037c90990b41eba974
Eric Jeliaz Chtilianov <echtilia@hydra10.eecs.utk.edu>
2024-09-03 12:09:38 -0400

    Implemented push_front function. Not sure how to default the number to 0 if stoi fails. Might have to do some excpetion work?

--- volsort.h
+++ volsort.h
@@ -5,33 +5,42 @@
 
 #include <string>
 
-struct Node {
-    std::string string;
-    int         number;
-    Node       *next;
+struct Node
+{
+  std::string string;
+  int number;
+  Node *next;
 };
 
-struct List {
-    Node       *head;
-    size_t      size;
+struct List
+{
+  Node *head;
+  size_t size;
 
-    List(); 					// define in list.cpp
-    ~List();					// define in list.cpp
+  List();  // define in list.cpp
+  ~List(); // define in list.cpp
 
-  //    void push_front(const std::string &s);	//define below
+  void push_front(const std::string &s)
+  {
+    Node *newNode = new Node();
+    newNode->string = s;
+    newNode->number = stoi(s);
+    newNode->next = head;
+    head = newNode;
+    size++;
+  }
 };
-
 
 // Functions -------------------------------------------------------------------
 
-bool node_number_compare(const Node *a, const Node *b); 	//implement in this file (volsort.h), used by quick, merge and stl
-bool node_string_compare(const Node *a, const Node *b);		//implement in this file (volsort.h), used by quick, merge and stl
+bool node_number_compare(const Node *a, const Node *b); // implement in this file (volsort.h), used by quick, merge and stl
+bool node_string_compare(const Node *a, const Node *b); // implement in this file (volsort.h), used by quick, merge and stl
 
-void dump_node(Node *n);					// implement in this file (volsort.h) to make it easier for TAs to grade
+void dump_node(Node *n); // implement in this file (volsort.h) to make it easier for TAs to grade
 
-void stl_sort(List &l, bool numeric);	// define in stl.cpp - sort using std::sort
-void qsort_sort(List &l, bool numeric);	// define in qsort.cpp - sort using qsort from cstdlib
-void merge_sort(List &l, bool numeric);	// define in merge.cpp - your implementation
-void quick_sort(List &l, bool numeric);	// define in quick.cpp - your implementation
+void stl_sort(List &l, bool numeric);   // define in stl.cpp - sort using std::sort
+void qsort_sort(List &l, bool numeric); // define in qsort.cpp - sort using qsort from cstdlib
+void merge_sort(List &l, bool numeric); // define in merge.cpp - your implementation
+void quick_sort(List &l, bool numeric); // define in quick.cpp - your implementation
 
 #endif
