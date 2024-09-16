e7dfa7a2c970e00b526ed7f5c41ad070ff64704f
Ethan Joseph Dietrich <edietri2@tesla11.eecs.utk.edu>
2024-09-16 08:05:04 -0400

    Qsort running

--- qsort.cpp
+++ qsort.cpp
@@ -2,76 +2,71 @@
 
 #include "volsort.h"
 
-#include <cstdlib>
-#include <array>
+#include <cstdlib> // for qsort
+#include <cstring> // for strcmp
 #include <iostream>
+#include <vector>
 
+using namespace std;
 
-
-void qsort_sort(List &l, bool numeric) {
-	
-	
-	/* STL SORT COPIED
-	 * Change STL sort call to Qsort call 
-	 * Create compare fxn for int and string
-	 */
-	
-	//Functions needed for qsort
-	/*
-	int compareint (const void * a, const void * b)
-	{
-		return ( *(int*)a - *(int*)b );
-	}
-
-	int comparestring (const void *arg1, const void *arg2) {
-		return strcmp( *(char * const *) arg1, *(char * const *) arg2);
-	}
-	*/
-
-	Node *current = l.head;
-    vector<int> sorted;
-    vector<string> stringSorted;
-
-    if (numeric)
-    {
-        while (current != NULL)
-        {
-            sorted.push_back(current->number);
-            current = current->next;
-        }
-		// differs from STL
-        qsort(sorted.begin(), sorted.end(), compareint());
-        current = l.head;
-
-        int i = 0;
-        while (current != NULL)
-        {
-            current->number = sorted[i];
-            current = current->next;
-            i++;
-        }
-    }
-    else
-    {
-        while (current != NULL)
-        {
-            stringSorted.push_back(current->string);
-            current = current->next;
-        }
-		// differs from STL
-        qsort(stringSorted.begin(), stringSorted.end(), comparestring());
-        current = l.head;
-
-        int i = 0;
-        while (current != NULL)
-        {
-            current->string = sorted[i];
-            current = current->next;
-            i++;
-        }
-    }
-
-	// end of STL paste
-
+// Comparison function for numeric sorting
+int compare_numeric(const void *a, const void *b)
+{
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
+	return (num1 - num2);
 }
 
+// Comparison function for string sorting
+int compare_string(const void *a, const void *b)
+{
+	string *str1 = (string *)a;
+	string *str2 = (string *)b;
+	return strcmp(str1->c_str(), str2->c_str());
+}
+
+void qsort_sort(List &l, bool numeric)
+{
+	Node *current = l.head;
+	vector<int> sorted;
+	vector<string> stringSorted;
+
+	if (numeric)
+	{
+		while (current != NULL)
+		{
+			sorted.push_back(current->number);
+			current = current->next;
+		}
+		// Use qsort for numeric sorting
+		qsort(&sorted[0], sorted.size(), sizeof(int), compare_numeric);
+
+		current = l.head;
+		int i = 0;
+		while (current != NULL)
+		{
+			current->number = sorted[i];
+			current = current->next;
+			i++;
+		}
+	}
+	else
+	{
+		while (current != NULL)
+		{
+			stringSorted.push_back(current->string);
+			current = current->next;
+		}
+		// Use qsort for string sorting
+		qsort(&stringSorted[0], stringSorted.size(), sizeof(string), compare_string);
+
+		current = l.head;
+		int i = 0;
+		while (current != NULL)
+		{
+			current->string = stringSorted[i];
+			current = current->next;
+			i++;
+		}
+	}
+}
